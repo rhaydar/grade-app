a2ca47e7c22b6533f9bb7d3498239a3332dfc958
Rashedul Haydar <rh2712@columbia.edu>
2017-04-09 15:30:25 -0400

    Adds responsive landing page

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var app = express();
 // set view engine to be ejs
 app.set("view engine", "ejs");
 
+// use public directory for static files
+app.use(express.static(__dirname + "/public"));
+
 // root path, homepage
 app.get("/", function(req, res) {
 	res.render("landing.ejs");
@@ -13,5 +16,5 @@ app.get("/", function(req, res) {
 
 // listen for connections
 app.listen(4000, function() {
-	console.log("Grading app server running");
+	console.log("Grading app server running on port 4000");
 });
